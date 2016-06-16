3670f2442fdd7cd062ea81b92a25ad0b64b27908
Andriy Oblivantsev <eslider@gmail.com>
2016-06-16 11:22:35 +0200

    Improve SeleniumPhantomJsTest and TestBase tests

--- src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
+++ src/Mapbender/CoreBundle/Tests/SeleniumPhantomJsTest.php
@@ -2,25 +2,78 @@
 
 namespace Mapbender\CoreBundle\Tests\Controller;
 
-class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase {
+use Symfony\Component\Process\Process;
 
-    public function setUp() {
-        if(PHP_MINOR_VERSION == 3) {
+/**
+ * Class SeleniumPhantomJsTest
+ */
+class SeleniumPhantomJsTest extends \PHPUnit_Extensions_Selenium2TestCase
+{
+    const PHANTOM_JS_PORT = 9876;
+    const PHANTOM_JS_HOST = 'localhost';
+
+    protected static $output;
+    /** @var  Process */
+    protected static $webServerProcces;
+    /** @var  Process */
+    protected static $phantomJsProccess;
+
+    public static function setUpBeforeClass()
+    {
+        /** @var Process $phantom */
+        $phantom                = self::$phantomJsProccess = new Process("phantomjs --webdriver=" . self::PHANTOM_JS_PORT);
+        self::$webServerProcces = new Process("app/console server:run");
+        self::$webServerProcces->start();
+        self::$phantomJsProccess->start();
+        //$o = array();
+        //$o[] = self::exec("app/console server:run");
+        //$o[]          = self::exec("phantomjs --webdriver=" . self::PHANTOM_JS_PORT);
+        //self::$output = $o;
+        // TODO: check if phantom js is running...
+    }
+
+    public static function tearDownAfterClass()
+    {
+        // TODO: stop phantomjs and symfony PHP server:run
+        self::$phantomJsProccess->stop(0, SIGKILL);
+        self::$webServerProcces->stop();
+    }
+
+    /**
+     * @param string $cmd
+     * @return string
+     */
+    protected static function exec($cmd)
+    {
+        return `$cmd &`;
+    }
+
+    public function setUp()
+    {
+        if (PHP_MINOR_VERSION == 3) {
             $this->markTestIncomplete('This test does not run on PHP 5.3.');
             return;
         }
-        $this->setHost('localhost');
-        $this->setPort(9876);
+        $this->setHost(self::PHANTOM_JS_HOST);
+        $this->setPort(self::PHANTOM_JS_PORT);
         $this->setBrowserUrl('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
     }
 
-    public function prepareSession() {
+    /**
+     * @return \PHPUnit_Extensions_Selenium2TestCase_Session
+     */
+    public function prepareSession()
+    {
         $res = parent::prepareSession();
         $this->url('/');
         return $res;
     }
 
-    public function testIndex() {
+    /**
+     *
+     */
+    public function testIndex()
+    {
         $this->url('http://' . TEST_WEB_SERVER_HOST . ':' . TEST_WEB_SERVER_PORT . '/app_dev.php/');
         $this->assertEquals('Applications', $this->title());
     }
--- src/Mapbender/CoreBundle/Tests/TestBase.php
+++ src/Mapbender/CoreBundle/Tests/TestBase.php
@@ -27,31 +27,34 @@ class TestBase extends WebTestCase
     /** @var CmdApplication Command application */
     protected static $application;
 
+    /** @var Registry */
+    protected static $doctrine;
+
+    /** @var  Connection */
+    protected static $connection;
+
+    /**
+     * The setUpBeforeClass() method
+     * called before the first test
+     * of the test case class is run respectively.
+     */
     public static function setUpBeforeClass()
     {
         self::$options     = $options = array();
         self::$client      = $client = static::createClient($options);
         self::$kernel      = $kernel = $client->getKernel();
         self::$application = $application = new CmdApplication($kernel);
+        self::$doctrine    = $doctrine = $client->getContainer()->get('doctrine');
+        self::$connection  = $connection = $doctrine->getConnection();
+
+        $appRootPath           = $kernel->getRootDir();
+        $configPath            = $appRootPath . "/config/";
+        $configurationBaseFile = $configPath . "parameters.yml.dist";
+        $configurationFile     = $configPath . "parameters.yml";
+        $isNewInstall          = !file_exists($configurationFile);
+        $isTestEnv             = $kernel->getEnvironment() == "test";
 
         $application->setAutoExit(false);
-    }
-
-    public function setUp()
-    {
-        static $kernel = null;
-
-        if ($kernel) {
-            return;
-        }
-
-        $kernel      = $this->getContainer()->get("kernel");
-        $appRootPath = $kernel->getRootDir();
-        $configPath  = $appRootPath . "/config/";
-        $configurationBaseFile = $configPath . "parameters.yml.dist";
-        $configurationFile = $configPath . "parameters.yml";
-        $isNewInstall = !file_exists($configurationFile);
-        $isTestEnv = $kernel->getEnvironment() == "test";
 
         if ($isNewInstall) {
             \ComposerBootstrap::allowWriteLogs();
@@ -59,18 +62,20 @@ class TestBase extends WebTestCase
             \ComposerBootstrap::clearCache();
         }
 
-
-        $connection = $this->getConnection();
         $params      = $connection->getParams();
         $hasDatabase = file_exists($params["path"]);
-        if($params["driver"] == "pdo_sqlite" && !$hasDatabase){
-            //$this->runCommand('doctrine:database:drop --force');
-            $this->runCommand('doctrine:database:create');
-            $this->runCommand('doctrine:schema:create');
-            $this->runCommand('fom:user:resetroot --username=root --password=root --email=root@example.com --silent');
-            $this->runCommand('doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append');
-            $this->runCommand('doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append');
+        if ($params["driver"] == "pdo_sqlite" && !$hasDatabase) {
+            self::runCommand('doctrine:database:drop --force');
+            self::runCommand('doctrine:database:create');
+            self::runCommand('doctrine:schema:create');
+            self::runCommand('fom:user:resetroot --username=root --password=root --email=root@example.com --silent');
+            self::runCommand('doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append');
+            self::runCommand('doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append');
         }
+    }
+
+    public static function tearDownAfterClass()
+    {
     }
 
 
@@ -84,16 +89,14 @@ class TestBase extends WebTestCase
         return self::$application;
     }
 
-
     /**
      * @param $command
      * @return mixed
      */
-    protected function runCommand($command)
+    protected static function runCommand($command)
     {
-        $command     = sprintf('%s --quiet', $command);
-        $application = $this->getApplication();
-        return $application->run(new StringInput($command));
+        $command = sprintf('%s --quiet', $command);
+        return self::$application->run(new StringInput($command));
     }
 
     /**
@@ -124,16 +127,33 @@ class TestBase extends WebTestCase
     /**
      * @return Connection
      */
-    private function getConnection()
+    protected function getConnection()
     {
-        return $this->getDoctrine()->getConnection();
+        return self::$connection;
     }
 
     /**
      * @return Registry
      */
-    private function getDoctrine()
+    protected function getDoctrine()
     {
-        return $this->getContainer()->get('doctrine');
+        return self::$doctrine;
+    }
+
+    /**
+     * @param  string $method GET|POST etc.
+     * @param  string $uri    URI absolute to application
+     * @param array   $parameters
+     * @param array   $files
+     * @param array   $server
+     * @param null    $content
+     * @param bool    $changeHistory
+     * @return null|\Symfony\Component\HttpFoundation\Response
+     */
+    public function requestAndGetResponse($method, $uri, array $parameters = array(), array $files = array(), array $server = array(), $content = null, $changeHistory = true)
+    {
+        $client  = $this->getClient();
+        $crawler = $client->request($method, $uri, $parameters, $files, $server, $content, $changeHistory);
+        return $client->getResponse()->getContent();
     }
 }
